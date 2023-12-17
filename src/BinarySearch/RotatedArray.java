149f7aa6a4ae8a508de42c97d59eb4ec4e4750f6
sachit <sachit10252parida@gmail.com>
2023-12-17 11:13:13 +0530

    Binary Search: Rotated Array problem

---
+++ src/BinarySearch/RotatedArray.java
@@ -0,0 +1,26 @@
+package BinarySearch;
+
+public class RotatedArray {
+    public static void main(String[] args) {    //Incomplete solution
+
+        int[] arr={3,4,5,6,8,0,1,2};
+
+        int maxValue=binarySearchForMax(arr);
+        System.out.println(args[maxValue]);
+    }
+
+    private static int binarySearchForMax(int[] arr) {
+        int start=0, mid=0, end=arr.length-1;
+
+        while(start<end){
+
+            mid=start+(end-start)/2;
+            if(arr[mid+1]<arr[mid])
+                return mid;
+            else
+                start= mid+1;
+        }
+        System.out.println(end);
+        return -1;
+    }
+}
