da65c0ea9d799210cec5b29bf0b7724c309160c3
sachit <sachit10252parida@gmail.com>
2024-02-09 22:27:07 +0530

    Frequently asked Java coding questions part-2

--- src/Practice/Test.java
+++ src/Practice/Test.java
@@ -205,8 +205,325 @@ public class Test {
          */
 
         /*
+            char array to string to list then perform operations
+
+            char[] arr={'1','3','5','3'};
+            String str= Arrays.toString(arr);
+            int frequency = Collections.frequency(Arrays.stream(str.split("")).toList(), "3");
+            System.out.println(frequency);
+         */
+
+        /*
+            Convert Integer[] to ArrayList<Integer> type
+
+            Integer[] arr1={1,2,3,4,5};
+            Integer[] arr2={2,3,1,0,5};
+
+            ArrayList<Integer> list1= new ArrayList<>(List.of(arr1));
+            ArrayList<Integer> list2= new ArrayList<>(List.of(arr2));
+         */
+
+        /*
+            Rotate the array by kth place (right rotation)
+
+            int[] arr={1,2,3,4,5};
+            // [4,5,1,2,3]
+            int k=2;
+            k=k%arr.length;
+
+            reverse(arr,0, arr.length-1);
+            reverse(arr,0,k-1);                 // reverse(arr,0,k); for left rotation
+            reverse(arr,k,arr.length-1);        //reverse(arr,k+1,arr.length-1);
+
+            System.out.println(Arrays.toString(arr));
+
+    }
+            static void reverse(int[] arr, int start, int end){
+            while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+         */
+
+        /*
+        Find pairs in array with a given sum
+        int[] arr={1,7,3,5,8,2,4,6,9};
+
+        Arrays.sort(arr); // 1 2 3 4 5 6 7 8 9
+        int start=0;
+        int end= arr.length-1;
+        int sum=0;
+        while(start<end){
+            if(arr[start]+arr[end]>sum)
+                end--;
+            else if(arr[start]+arr[end]<sum)
+                start++;
+            else if(arr[start]+arr[end]==sum)
+            {
+                System.out.println("pair ("+arr[start]+","+arr[end]+")");
+                start++;end--;
+            }
+            else System.out.println("Not a valid combo");
 
          */
+
+        /*
+        Remove duplicate elements
+
+
+        Integer[] arr={11,1,1,23,2,2,2,56,4,66,77,66,90};
+        ArrayList<Integer> list= new ArrayList<>(Arrays.asList(arr));
+
+        HashSet<Integer> hs= new HashSet<>();
+        List<Integer> uniqueList = list.stream().filter(x -> hs.add(x)).toList();
+        System.out.println(uniqueList);
+         */
+
+        /*
+        One string is a rotated version of another string
+
+        String str1="abc";// abc   bc   abcbc
+        String str2="bca";
+        String str3=str1+str1;
+        if(str3.contains(str2))
+        System.out.println("Rotated");
+        else System.out.println("Not rotated");
+         */
+
+        /*
+         Intersection of two arrays
+
+         Integer[] arr1={1,2,3,4,5};
+        Integer[] arr2={2,34,4,45,66};
+
+        ArrayList<Integer> list1= new ArrayList<>(Arrays.asList(arr1));
+        ArrayList<Integer> list2= new ArrayList<>(Arrays.asList(arr2));
+
+        List<Integer> result = list1.stream().filter(list2::contains).toList();
+        System.out.println(result);
+         */
+
+
+        /*
+         Reverse each work of a string
+
+         String str="Java Concept Of The Day";
+
+         String revString = Arrays.stream(str.split(" "))
+                .map(x -> new StringBuilder(x).reverse()).collect(Collectors.joining(" "));
+
+         System.out.println(revString);
+         */
+
+        /*
+            Digit or not
+            char ch = inputString.charAt(i);
+            if (Character.isDigit(ch))
+         */
+
+
+        /*
+            Deep to String is used to print dimensional array
+
+            String[][] twoDArray = new String[][] {
+                {"ONE", "TWO", "THREE", "FOUR"},
+                {"FIVE", "SIX", "SEVEN"},
+                {"EIGHT", "NINE", "TEN", "ELEVEN", "TWELVE"}
+                };
+                System.out.println(Arrays.deepToString(twoDArray));
+         */
+
+        /*
+                Converting ArrayList to Array
+
+                ArrayList<Integer> list= new ArrayList<>(Arrays.asList(1,2,3,4,5));
+                Object[] array = list.toArray();
+                System.out.println(Arrays.toString(array));
+         */
+
+        /*
+                More on ArrayList
+
+                //Retrieving a SubList
+                 List<Integer> subList = list.subList(1, 4);
+
+                 //Removing all elements of the list
+                    list.clear();
+
+
+
+         */
+
+        /*
+        Traversing LinkedList in reverse order
+
+        //Getting the Iterator object using descendingIterator() method
+
+        Iterator<String> it = list.descendingIterator();
+
+        //printing the elements of list in reverse order
+
+        while (it.hasNext())
+        {
+            System.out.println(it.next());
+        }
+         */
+
+        /*
+        LinkedList as queue: queue.offer(10)   queue.poll();
+        LinkedList as stack: stack.push(10)    stack.pop();
+         */
+
+
+        /*
+        PriorityQueue is naturally polled in ascending order
+
+        //MyComparator Class
+
+        class MyComparator implements Comparator<Employee>
+        {
+         @Override
+        public int compare(Employee e1, Employee e2)
+        {
+        return e1.salary - e2.salary;
+        }
+
+        MyComparator comparator = new MyComparator();
+        PriorityQueue<Employee> pQueue = new PriorityQueue<Employee>(7, comparator);
+        pQueue.offer(new Employee("AAA", 15000));
+
+        pQueue.offer(new Employee("BBB", 12000));
+
+        pQueue.offer(new Employee("CCC", 7500));
+
+        pQueue.offer(new Employee("DDD", 17500));
+
+        pQueue.offer(new Employee("EEE", 21500));
+
+        pQueue.offer(new Employee("FFF", 29000));
+
+        pQueue.offer(new Employee("GGG", 14300));
+
+        System.out.println(pQueue.poll());       //Output --> CCC : 7500
+
+        System.out.println(pQueue.poll());       //Output --> BBB : 12000
+
+        System.out.println(pQueue.poll());       //Output --> GGG : 14300
+
+        System.out.println(pQueue.poll());       //Output --> AAA : 15000
+
+        System.out.println(pQueue.poll());       //Output --> DDD : 17500
+
+        System.out.println(pQueue.poll());       //Output --> EEE : 21500
+
+        System.out.println(pQueue.poll());       //Output --> FFF : 29000
+         */
+
+
+        /*
+            Synchronized ArrayList, HasSet, Hashmap
+
+             *****List<String> synchronizedList = Collections.synchronizedList(list);
+
+        //you must use synchronized block while iterating over synchronizedList
+
+        synchronized (synchronizedList)
+        {
+            Iterator<String> it = synchronizedList.iterator();
+
+            while (it.hasNext())
+            {
+                System.out.println(it.next());
+            }
+        }
+
+
+        ****
+        //Getting synchronized set
+
+        Set<String> synchronizedSet = Collections.synchronizedSet(set);
+
+        //you must use synchronized block while iterating over synchronizedSet
+
+        synchronized (synchronizedSet)
+        {
+            Iterator<String> it = synchronizedSet.iterator();
+
+            while (it.hasNext())
+            {
+                System.out.println(it.next());
+            }
+        }
+
+        **************
+        //Getting synchronized map
+
+        Map<String, Integer> synchronizedMap = Collections.synchronizedMap(map);
+
+        Set<String> keySet = synchronizedMap.keySet();
+
+        System.out.println("Keys.............");
+
+        //While iterating over synchronizedMap, you must use synchronized block.
+
+        synchronized (synchronizedMap)
+        {
+            Iterator<String> it = keySet.iterator();
+
+            while (it.hasNext())
+            {
+                System.out.println(it.next());
+            }
+        }
+         */
+
+        /*
+        Convert a list to unmodifiable list
+
+        ArrayList<Integer> list= new ArrayList<>(Arrays.asList(1,2,3,45));
+
+        List<Integer> ul=  Collections.unmodifiableList(list);
+        System.out.println(ul);
+         */
+
+        /*
+        Sort by key in Hashmap
+
+        Map<String, Integer> studentMap = new HashMap<String, Integer>();
+
+        studentMap.put("Jyous", 87);
+        studentMap.put("Klusener", 82);
+        studentMap.put("Xiangh", 91);
+        studentMap.put("Lisa", 89);
+        studentMap.put("Narayan", 95);
+        studentMap.put("Arunkumar", 86);
+
+        studentMap.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(System.out::println);
+
+         */
+
+        /*
+            Bubble sort algorithm
+
+                private static int[] bubblesortArr(int[] arr) {
+
+                int temp=0;
+                for (int i = 0; i < arr.length-1; i++) {
+                    for (int j = 0; j < arr.length-1-i; j++) {
+                        if(arr[j]>arr[j+1]){
+                                temp=arr[j];
+                                arr[j]=arr[j+1];
+                                arr[j+1]=temp;
+                    }
+
+                }
+            }
+        return arr;
     }
+         */
     }
+}
 
