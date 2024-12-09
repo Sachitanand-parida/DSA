ac96a94207ea912bebc865a424da85fba9622fd0
sachit <sachit10252parida@gmail.com>
2024-12-09 23:06:34 +0530

    Maze problem

---
+++ src/BackTracking/Maze.java
@@ -0,0 +1,17 @@
+package BackTracking;
+
+public class Maze {
+    public static void main(String[] args) {
+
+        System.out.println(maze(3,3));
+    }
+
+    static int maze(int row, int col){
+        if(row==1 || col==1)
+            return 1;
+
+        int left= maze(row-1, col);
+        int right= maze(row, col-1);
+        return left+ right;
+    }
+}
