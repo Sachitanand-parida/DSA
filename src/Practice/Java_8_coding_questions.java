4e9597cbc57f467329d650d3371c467432488930
Sachit <sachit10252parida@gmail.com>
2026-07-16 17:53:39 +0530

    added more java 8 coding questions

--- src/Practice/Java_8_coding_questions.java
+++ src/Practice/Java_8_coding_questions.java
@@ -1,11 +1,6 @@
 package Practice;
 
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-public class Test {
+public class Java_8_coding_questions {
     public static void main(String[] args) {
 
 
